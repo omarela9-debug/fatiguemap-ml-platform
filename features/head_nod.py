d61346a9efa39fe13ee1887ed5e96f321caf8151
omarela9-debug <omarela9@gmail.com>
2026-04-28 22:17:59 -0400

    Update head_nod.py

--- features/head_nod.py
+++ features/head_nod.py
@@ -1,1 +1,11 @@
+def detect_head_nod(current_pitch, previous_pitch, threshold=8.0):
+    """
+    Detects a possible head nod event using pitch angle change.
+    """
 
+    pitch_change = abs(current_pitch - previous_pitch)
+
+    if pitch_change >= threshold:
+        return True
+
+    return False
