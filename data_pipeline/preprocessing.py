28a188208ea0962f3a69ba6c00567989a721ad16
omarela9-debug <omarela9@gmail.com>
2026-04-28 22:20:30 -0400

    Update preprocessing.py

--- data_pipeline/preprocessing.py
+++ data_pipeline/preprocessing.py
@@ -1,1 +1,9 @@
+def normalize_signal(value, min_value, max_value):
+    """
+    Normalizes a sensor value to a 0.0-1.0 range.
+    """
 
+    if max_value == min_value:
+        return 0.0
+
+    return round((value - min_value) / (max_value - min_value), 3)
