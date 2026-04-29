4739c4dd907afb216ddd22808dd5f1713073d2ce
omarela9-debug <omarela9@gmail.com>
2026-04-28 22:19:53 -0400

    Update blink_rate.py

--- features/blink_rate.py
+++ features/blink_rate.py
@@ -1,1 +1,9 @@
+def calculate_blink_rate(blink_count, time_window_seconds):
+    """
+    Calculates blinks per minute.
+    """
 
+    if time_window_seconds == 0:
+        return 0.0
+
+    return round((blink_count / time_window_seconds) * 60, 2)
