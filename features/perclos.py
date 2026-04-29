cd6773877667774c0e510980d7374e76d336b591
omarela9-debug <omarela9@gmail.com>
2026-04-28 22:18:16 -0400

    Update perclos.py

--- features/perclos.py
+++ features/perclos.py
@@ -1,1 +1,9 @@
+def calculate_perclos(closed_eye_frames, total_frames):
+    """
+    Calculates PERCLOS / frame closure ratio.
+    """
 
+    if total_frames == 0:
+        return 0.0
+
+    return round(closed_eye_frames / total_frames, 3)
